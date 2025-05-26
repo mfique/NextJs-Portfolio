8a8f877b9e0f4e8a5018478c64359b281a0b6ee9
mfique <pacifiquem58@gmail.com>
2025-05-26 08:13:36 +0200

    Update title to Mugisha Pacifique

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'My Portfolio',
+  title: 'Mugisha Pacifique',
   description: 'Welcome to my portfolio website',
 }
 
